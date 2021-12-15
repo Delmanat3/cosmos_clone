1eb823cb8379c53e149c10b636c83cb7d86578e1
Delmanat3 <88867653+Delmanat3@users.noreply.github.com>
2021-12-15 13:23:51 -0600

    removing unused js

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -4,6 +4,7 @@ import { useQuery } from "@apollo/client";
 import { GET_LOAD } from "../utils/queries";
 import Grid from "@mui/material/Grid";
 import { News } from "../components/News/News";
+import Auth from '../utils/auth';
 
 import { MidCenter } from "../components/hero";
 import SearchModal from "../components/Search/SearchModal";
@@ -11,12 +12,14 @@ import SearchModal from "../components/Search/SearchModal";
 export function Home() {
   // Trigger toggle using onChange Switch
 
-  const { data, error, loading } = useQuery(GET_LOAD);
 
-  if (error) throw new Error();
-  if (loading) return "loading....";
-  const { coins } = data;
-  console.log(coins);
+ const poke= Auth.isTokenExpired
+ if(poke==true){
+   alert('session expired')
+
+ }
+
+
   return (
     <div style={{ backgroundColor: "#262626" }}>
       <Grid
