828ccd099cfb0258aaa4413cc4930acc3771a1ec
Delmanat3 <88867653+Delmanat3@users.noreply.github.com>
2022-02-03 21:36:37 -0600

    still cant think of it. sw as well coz why not

--- client/src/App.js
+++ client/src/App.js
@@ -44,7 +44,23 @@ const client = new ApolloClient({
 
 
 function App() {
-
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+      navigator.serviceWorker.register("/serviceWorker.js").then(
+        function (registration) {
+          // Registration was successful
+          console.log(
+            "ServiceWorker registration successful with scope: ",
+            registration.scope
+          );
+        },
+        function (err) {
+          // registration failed :(
+          console.log("ServiceWorker registration failed: ", err);
+        }
+      );
+    });
+  }
 
   return (
       <ApolloProvider client={client}>
